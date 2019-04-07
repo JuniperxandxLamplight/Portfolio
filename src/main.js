a0f965196d5fea7c652b453f632118613bce0c6a
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-04-06 17:23:06 -0700

    add background ocean animation

--- src/main.js
+++ src/main.js
@@ -1,50 +1,19 @@
-import './css/Aurora-Framework.css';
 import './css/styles.css';
 import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { User, ageCalc, userLifeExpectancy } from './backEnd.js';
+// import { User, ageCalc, userLifeExpectancy } from './backEnd.js';
 
 $(function(){
-  $(".calculate").click(function(){
-    const day = parseInt($(".day").text());
-    const month = parseInt($(".month").val());
-    const year = parseInt($(".year").val());
-    const activity = parseInt($(".activity").val());
-    const sex = parseInt($(".sex").val());
-    const relationships = parseInt($(".relationships").val());
-    const smoker = parseInt($(".smoker").val());
-
-    let user = new User(month, day, year, activity, sex, relationships, smoker);
-    ageCalc(user);
-    userLifeExpectancy(user);
-
-    $(".form").fadeOut(500);
-    $(".earthAge").text(user.earthAge);
-    $(".mercuryAge").text(user.mercuryAge);
-    $(".venusAge").text(user.venusAge);
-    $(".marsAge").text(user.marsAge);
-    $(".jupiterAge").text(user.jupiterAge);
-    if (user.over === false){
-      $(".earthYearsLeft").text(user.earthYearsLeft);
-      $(".mercuryYearsLeft").text(user.mercuryYearsLeft);
-      $(".venusYearsLeft").text(user.venusYearsLeft);
-      $(".marsYearsLeft").text(user.marsYearsLeft);
-      $(".jupiterYearsLeft").text(user.jupiterYearsLeft);
-      $(".yearsLeft").fadeIn().delay(500);
-    } else if (user.over === true){
-      $(".earthYearsOver").text(user.earthYearsOver);
-      $(".mercuryYearsOver").text(user.mercuryYearsOver);
-      $(".venusYearsOver").text(user.venusYearsOver);
-      $(".marsYearsOver").text(user.marsYearsOver);
-      $(".jupiterYearsOver").text(user.jupiterYearsOver);
-      $(".yearsOver").fadeIn().delay(500);
-    }
-    $(".ageDisplay").fadeIn().delay(500);
-    $(".restart").fadeIn().delay(500);
-  });
-  $(".restart").click(function(){
-    location.reload();
-  });
+  for(let i = 0; i < 625; i++){
+    $('.container').append(`<div class='wave piece${i}'>
+      <div class='leftWave halfPiece${i} move${i}'></div>
+      <div class='rightWave halfPiece${i} move${i}'></div>
+      <div class='cover cover${i}'>
+      </div>
+    </div>`)
+  }
+  for(let i = 0; i < 625; i++){
+    $(`.move${i}`).css('animation-delay', `${Math.floor(Math.random() * 10)}s`);
+    $(`.piece${i}`).css('animation-delay', `${0.01*i}s`);
+    $(`.cover${i}`).css('animation-delay', `${0.01*i}s`);
+  }
 });
